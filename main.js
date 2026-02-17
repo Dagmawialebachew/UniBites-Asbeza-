9ff6b66a84f3b04a24f553ae871d441393558b35
Dagmawialebachew <dagmawialebachew@gmail.com>
2026-02-17 11:12:50 +0300

    updating the styling

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ let cart = [];
 
 async function loadItems() {
   const res = await fetch(`${API}/asbeza/items`);
-  const items = await res.json();
+const data = await res.json();
+const items = data.items;   // <-- grab the array
 
-  const container = document.getElementById("items");
+items.forEach(item => {
+  const div = document.createElement("div");
+  div.className = "bg-white p-4 rounded-xl shadow flex justify-between";
 
-  items.forEach(item => {
-    const div = document.createElement("div");
-    div.className = "bg-white p-4 rounded-xl shadow flex justify-between";
+  div.innerHTML = `
+    <div>
+      <h2 class="font-semibold">${item.name}</h2>
+      <p class="text-sm text-gray-500">${item.base_price} birr</p>
+    </div>
+    <button class="bg-black text-white px-3 py-1 rounded"
+      onclick='addToCart(${JSON.stringify(item)})'>
+      Add
+    </button>
+  `;
 
-    div.innerHTML = `
-      <div>
-        <h2 class="font-semibold">${item.name}</h2>
-        <p class="text-sm text-gray-500">${item.price} birr</p>
-      </div>
-      <button class="bg-black text-white px-3 py-1 rounded"
-        onclick='addToCart(${JSON.stringify(item)})'>
-        Add
-      </button>
-    `;
+  container.appendChild(div);
+});
 
-    container.appendChild(div);
-  });
 }
 
 function addToCart(item) {
